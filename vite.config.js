370bc90fa8c021be67f311898f63872ba579e739
1 <a1@MacBook-Pro.local>
2025-07-12 15:49:09 +0530

    Fix: Disabled tree-shaking to fix Vercel build error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: false, // disables minification
+    rollupOptions: {
+      treeshake: false, // disables tree-shaking (likely cause of error)
+    },
+  },
 })
+
